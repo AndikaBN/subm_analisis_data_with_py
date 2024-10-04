9a5002f27e6a3049378053f47695c8f5ee8e3d41
AndikaBN <bintangnursalih275@gmail.com>
2024-10-05 04:12:58 +0800

    revisi 1

--- Dashboard/dashboard.py
+++ Dashboard/dashboard.py
@@ -4,91 +4,9 @@ import matplotlib.image as mpimg
 import seaborn as sns
 import streamlit as st
 import urllib
+from function import DataAnalyzer, BrazilMapPlotter
 
-class DataAnalyzer:
-    def __init__(self, df):
-        self.df = df
-
-    def create_daily_orders_df(self):
-        daily_orders_df = self.df.resample(rule='D', on='order_approved_at').agg({
-            "order_id": "nunique",
-            "payment_value": "sum"
-        })
-        daily_orders_df = daily_orders_df.reset_index()
-        daily_orders_df.rename(columns={
-            "order_id": "order_count",
-            "payment_value": "revenue"
-        }, inplace=True)
-        
-        return daily_orders_df
-    
-    def create_sum_spend_df(self):
-        sum_spend_df = self.df.resample(rule='D', on='order_approved_at').agg({
-            "payment_value": "sum"
-        })
-        sum_spend_df = sum_spend_df.reset_index()
-        sum_spend_df.rename(columns={
-            "payment_value": "total_spend"
-        }, inplace=True)
-
-        return sum_spend_df
-
-    def create_sum_order_items_df(self):
-        sum_order_items_df = self.df.groupby("product_category_name_english")["product_id"].count().reset_index()
-        sum_order_items_df.rename(columns={
-            "product_id": "product_count"
-        }, inplace=True)
-        sum_order_items_df = sum_order_items_df.sort_values(by='product_count', ascending=False)
-
-        return sum_order_items_df
-
-    def review_score_df(self):
-        review_scores = self.df['review_score'].value_counts().sort_values(ascending=False)
-        most_common_score = review_scores.idxmax()
-
-        return review_scores, most_common_score
-
-    def create_bystate_df(self):
-        bystate_df = self.df.groupby(by="customer_state").customer_id.nunique().reset_index()
-        bystate_df.rename(columns={
-            "customer_id": "customer_count"
-        }, inplace=True)
-        most_common_state = bystate_df.loc[bystate_df['customer_count'].idxmax(), 'customer_state']
-        bystate_df = bystate_df.sort_values(by='customer_count', ascending=False)
-
-        return bystate_df, most_common_state
-
-    def create_order_status(self):
-        order_status_df = self.df["order_status"].value_counts().sort_values(ascending=False)
-        most_common_status = order_status_df.idxmax()
-
-        return order_status_df, most_common_status
-    
-class BrazilMapPlotter:
-    def __init__(self, data, plt, mpimg, urllib, st):
-        self.data = data
-        self.plt = plt
-        self.mpimg = mpimg
-        self.urllib = urllib
-        self.st = st
-
-    def plot(self):
-        brazil = self.mpimg.imread(self.urllib.request.urlopen('https://i.pinimg.com/originals/3a/0c/e1/3a0ce18b3c842748c255bc0aa445ad41.jpg'),'jpg')
-        ax = self.data.plot(kind="scatter", x="geolocation_lng", y="geolocation_lat", figsize=(10,10), alpha=0.3,s=0.3,c='maroon')
-        self.plt.axis('off')
-        self.plt.imshow(brazil, extent=[-73.98283055, -33.8,-33.75116944,5.4])
-        self.st.pyplot()
-
-st.markdown("""
-    <style>
-    .css-1d391kg {
-        background-color: black !important;
-    }
-    .css-1d391kg h1, .css-1d391kg h2, .css-1d391kg h3, .css-1d391kg h4, .css-1d391kg h5, .css-1d391kg h6, .css-1d391kg p {
-        color: white !important;
-    }
-    </style>
-    """, unsafe_allow_html=True)
+sns.set(style='dark')
 
 # Dataset
 datetime_cols = ["order_approved_at", "order_delivered_carrier_date", "order_delivered_customer_date", "order_estimated_delivery_date", "order_purchase_timestamp", "shipping_limit_date"]
@@ -112,8 +30,7 @@ with st.sidebar:
     with col1:
         st.write(' ')
     with col2:
-        st.image("WTCf.png"
-                 , width=100)
+        st.image("WTCf.png", width=100)
     with col3:
         st.write(' ')
 
@@ -140,10 +57,10 @@ state, most_common_state = function.create_bystate_df()
 order_status, common_status = function.create_order_status()
 
 # Define your Streamlit app
-st.title("Analisis Data Publik E-Commerce")
+st.title("E-Commerce Public Data Analysis")
 
 # Add text or descriptions
-st.write("**Dasbor untuk menganalisis data publik E-Commerce.**")
+st.write("**This is a dashboard for analyzing E-Commerce public data.**")
 
 # Daily Orders Delivered
 st.subheader("Daily Orders Delivered")
@@ -163,11 +80,15 @@ sns.lineplot(
     y=daily_orders_df["order_count"],
     marker="o",
     linewidth=2,
-    color="#90CAF9"
+    color="#90CAF9",
+    ax=ax
 )
 ax.tick_params(axis="x", rotation=45)
 ax.tick_params(axis="y", labelsize=15)
+
+# Pass the figure to st.pyplot
 st.pyplot(fig)
+
 
 # Customer Spend Money
 st.subheader("Customer Spend Money")
@@ -188,12 +109,16 @@ sns.lineplot(
     y="total_spend",
     marker="o",
     linewidth=2,
-    color="#90CAF9"
+    color="#90CAF9",
+    ax=ax
 )
 
 ax.tick_params(axis="x", rotation=45)
 ax.tick_params(axis="y", labelsize=15)
+
+# Pass the figure to st.pyplot
 st.pyplot(fig)
+
 
 # Order Items
 st.subheader("Order Items")
@@ -207,26 +132,19 @@ with col2:
     avg_items = sum_order_items_df["product_count"].mean()
     st.markdown(f"Average Items: **{avg_items}**")
 
-fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(45, 25))
+fig, (ax1, ax2) = plt.subplots(nrows=1, ncols=2, figsize=(22.5, 12.5))
 
-sns.barplot(x="product_count", y="product_category_name_english", data=sum_order_items_df.head(5), palette="viridis", ax=ax[0])
-ax[0].set_ylabel(None)
-ax[0].set_xlabel("Number of Sales", fontsize=80)
-ax[0].set_title("Most sold products", loc="center", fontsize=90)
-ax[0].tick_params(axis ='y', labelsize=55)
-ax[0].tick_params(axis ='x', labelsize=50)
+sns.barplot(x="product_count", y="product_category_name_english", data=sum_order_items_df.head(5), palette="viridis", ax=ax1)
+sns.barplot(x="product_count", y="product_category_name_english", data=sum_order_items_df.sort_values(by="product_count", ascending=True).head(5), palette="viridis", ax=ax2)
 
-sns.barplot(x="product_count", y="product_category_name_english", data=sum_order_items_df.sort_values(by="product_count", ascending=True).head(5), palette="viridis", ax=ax[1])
-ax[1].set_ylabel(None)
-ax[1].set_xlabel("Number of Sales", fontsize=80)
-ax[1].invert_xaxis()
-ax[1].yaxis.set_label_position("right")
-ax[1].yaxis.tick_right()
-ax[1].set_title("Fewest products sold", loc="center", fontsize=90)
-ax[1].tick_params(axis='y', labelsize=55)
-ax[1].tick_params(axis='x', labelsize=50)
+# Adjust axes properties
+ax1.set_xlabel("Number of Sales", fontsize=16)
+ax1.set_title("Most sold products", loc="center", fontsize=18)
+ax2.invert_xaxis()
 
+# Pass the figure to st.pyplot
 st.pyplot(fig)
+
 
 # Review Score
 st.subheader("Review Score")
@@ -241,24 +159,19 @@ with col2:
     st.markdown(f"Most Common Review Score: **{most_common_review_score}**")
 
 fig, ax = plt.subplots(figsize=(12, 6))
-colors = sns.color_palette("viridis", len(review_score))
-
-sns.barplot(x=review_score.index,
-            y=review_score.values,
-            order=review_score.index,
-            palette=colors)
+sns.barplot(x=review_score.index, y=review_score.values, palette="viridis", ax=ax)
 
 plt.title("Customer Review Scores for Service", fontsize=15)
 plt.xlabel("Rating")
 plt.ylabel("Count")
-plt.xticks(fontsize=12)
-plt.yticks(fontsize=12)
 
-# Menambahkan label di atas setiap bar
+# Add labels on bars
 for i, v in enumerate(review_score.values):
     ax.text(i, v + 5, str(v), ha='center', va='bottom', fontsize=12, color='black')
 
+# Pass the figure to st.pyplot
 st.pyplot(fig)
+
 
 # Customer Demographic
 st.subheader("Customer Demographic")
@@ -268,26 +181,16 @@ with tab1:
     most_common_state = state.customer_state.value_counts().index[0]
     st.markdown(f"Most Common State: **{most_common_state}**")
 
-    # Create figure and axis
     fig, ax = plt.subplots(figsize=(12, 6))
-    
-    # Plot the bar plot on the axis
-    sns.barplot(
-        x=state.customer_state.value_counts().index,
-        y=state.customer_count.values, 
-        data=state,
-        palette="viridis",
-        ax=ax  # Specify the axis object for the plot
-    )
+    sns.barplot(x=state.customer_state.value_counts().index, y=state.customer_count.values, palette="viridis", ax=ax)
 
-    # Set title and labels on the axis object
-    ax.set_title("Number of Customers from State", fontsize=15)
-    ax.set_xlabel("State", fontsize=12)
-    ax.set_ylabel("Number of Customers", fontsize=12)
-    ax.tick_params(axis='x', labelsize=12)
+    plt.title("Number of customers from State", fontsize=15)
+    plt.xlabel("State")
+    plt.ylabel("Number of Customers")
 
-    # Display the plot using st.pyplot, passing the figure
+    # Pass the figure to st.pyplot
     st.pyplot(fig)
+
 
 with tab2:
     map_plot.plot()
